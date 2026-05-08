4340f9aa389f4044c056bdb95bf9b183a6f2b54e
Matt Braniff <mbraniff@rocketmail.com>
2026-05-08 17:18:37 -0600

    Initial Commit

---
+++ ImageViewingApp/ImageViewingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewingAppApp.swift
+//  ImageViewingApp
+//
+//  Created by Matthew Braniff on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
